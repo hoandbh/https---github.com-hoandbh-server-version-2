a9796789184048a824275a84992f2ac0f733224e
hoandbh <project2023bh@gmail.com>
2023-02-13 22:33:59 +0200

    22:33 13.2 b

---
+++ routes/possibleAnsRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const possibleAnsRouter = express.Router();
+const pAnsController = require('../controllers/possibleAnsController');
+
+
+
+possibleAnsRouter.route('/')
+.get(pAnsController.getAllPossibleAnswers)
+.post(pAnsController.createPossibleAnswer)
+
+
+
+//localhost:3600/possible_answer/444
+possibleAnsRouter.route('/:id')
+.get(pAnsController.getPossibleAnsById)
+.delete(pAnsController.delete)
