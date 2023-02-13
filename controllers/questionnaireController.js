1cc393ff61464fdb198300b60eee365946b5955f
hoandbh <project2023bh@gmail.com>
2023-02-13 13:39:24 +0200

    13:39 13_2 Bracha

--- controllers/questionnaireController.js
+++ controllers/questionnaireController.js
@@ -5,7 +5,20 @@ class QuestionnaireController {
     //localhost:3600/api/messages
 
     getAllQuestionnaires = async(req,res)=>{
+        const questionnaires = await questoinnaireDal.getAllQuestionnaires();
+        if(!messages?.length)
+            return res.status(400).json({message:'No questionnaires found'})
+        res.json(questionnaires)
+        
+    }
+    getQuestionnaire = async(req,res)=>{
         const parameters = req.query;
+        const questionnaires = await questoinnaireDal.getAllQuestionnaires();
         
     }
 }
+
+
+const questionnaireController = new QuestionnaireController();
+
+module.exports = questionnaireController;
--- dal/questionnaireDal.js
+++ dal/questionnaireDal.js
@@ -6,6 +6,33 @@ class QuestionnaireDal {
 
     getAllQuestionnaires = async() =>{
         const quests = await Questionnaire.findAll({});
-        return this.getAllQuestionnaires;
+        return quests;
     }
+  
+    getQuestionnaireById = async(id)=>{
+        var quest = await Questionnaire.findOne({
+            where :{
+                id_questionnaire: id
+            }
+        })
+        return quest;
+    }
+    //add more functions for getting according to certain paramters
+    createNewQuestionnaire = async(content)=>{
+        const quest = await Questionnaire.create(content);
+        return quest;
+    }
+    deleteQuestionnaire = async(id)=>{
+        await Questionnaire.destroy({
+            where: {
+                id_questionnaire: id
+            }
+        })
+    }
+
+    //TODO updateQuestionnaire = async()=>
 }
+
+
+const questionnaireDal = new QuestionnaireDal();
+module.exports = questionnaireDal;
