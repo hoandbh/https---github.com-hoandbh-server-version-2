7e8d1a1cd099a26da512c91ffc2666ed564a28c3
Hadas-lab <36214128472@mby.co.il>
2023-02-15 17:57:07 +0200

    hadas

--- models/extra_setup.js
+++ models/extra_setup.js
@@ -2,13 +2,8 @@ const { sequelize } = require("./sequelize");
 
 const applyExtraSetup = () => {
 
-<<<<<<< HEAD
     const {course, message, qst_in_questionnaire, ans_in_version, possible_answer,
            qst_in_version,questionnaire,score, test, user, version} = sequelize.models;
-=======
-    const {courses, messages, qst_in_questionnaires, ans_in_versions, possible_answers,
-         qst_in_versions,questionnaire,scores, test, users, versions} = sequelize.models;
->>>>>>> 19a8adcf22d2ecdeec9ba5535bca418dce6b5ed9
         //  book.belongsTo(category, { foreignKey: "cateogry_id", as: "category" });
         //  book.belongsTo(author, { foreignKey: "author_id", as: "author" });
         //  author.hasMany(book, { foreignKey: "author_id", as: "books" });
@@ -20,26 +15,10 @@ const applyExtraSetup = () => {
         // });
         // message.belongsTo(user)
         // user.hasMany(message)
-         
-<<<<<<< HEAD
         // qst_in_questionnaire.belongsTo(questionnaire, {foreignKey: "questionnaire", as: "questionnaire"});
         // questionnaire.hasMany(qst_in_questionnaire,{foreignKey:"questionnaire", as: "questions"});
-
-
         // possible_answer.belongsTo(qst_in_questionnaire,{foreignKey:"qst", as: "question"});
         // qst_in_questionnaire.hasMany(possible_answer,{foreignKey:"qst", as: "answers" });
-=======
-       qst_in_questionnaires.belongsTo(questionnaire, {foreignKey: "questionnaire", as: "questionnaire_id"});
-       questionnaire.hasMany(qst_in_questionnaires,{foreignKey:"questionnaire", as: "questions"});
-
-
-        possible_answers.belongsTo(qst_in_questionnaires,{foreignKey:"qst", as: "question"});
-        qst_in_questionnaires.hasMany(possible_answers,{foreignKey:"qst", as: "answers" });
->>>>>>> 19a8adcf22d2ecdeec9ba5535bca418dce6b5ed9
-
-        
-
-
 }
 
 module.exports = { applyExtraSetup };
