ae804f26e03a61884159e32fadd62113ba8fa22c
hoandbh <project2023bh@gmail.com>
2023-02-18 19:54:09 +0200

    bracha2

--- models/user.js
+++ models/user.js
@@ -1,36 +1,38 @@
 const { sequelize, DataTypes } = require("./sequelize");
-    const User = sequelize.define(
-        "users",
-        {
-            id_user: {
-                type: DataTypes.INTEGER,
-                //,allowNull:
-                //autoIncrement: true,
-                primaryKey: true
-            },
-            user_name: {
-                type: DataTypes.STRING(1000)
-                //,allowNull:
-            },
-            password: {
-                type: DataTypes.STRING(1000)
-                //,allowNull:
-            },
-            email: {
-                type: DataTypes.STRING(1000)
-                //,allowNull:
-            },
-            permission:{
-                type:DataTypes.INTEGER                
-                //,allowNull:
-            },
-            course:{
-                type:DataTypes.INTEGER                
-                //,allowNull:
-            }
+const User = sequelize.define(
+    "users",
+    {
+        id_user: {
+            type: DataTypes.INTEGER,
+            //,allowNull:
+            //autoIncrement: true,
+            primaryKey: true
         },
-        {
-            timestamps: false,
+        user_name: {
+            type: DataTypes.STRING(1000)
+            //,allowNull:
+        },
+        password: {
+            type: DataTypes.STRING(1000)
+            //,allowNull:
+        },
+        email: {
+            type: DataTypes.STRING(1000)
+            //,allowNull:
+        },
+        permission: {
+            type: DataTypes.INTEGER
+            //,allowNull:
+        },
+        course: {
+            type: DataTypes.INTEGER
+            //,allowNull:
         }
-    );
+    },
+    {
+        timestamps: false,
+    }
+);
 module.exports = User;
+
+
