360efd7918f62278771c933e3bd21cdbe904a4df
hoandbh <project2023bh@gmail.com>
2023-02-22 22:41:42 +0200

    "bbb"

--- models/extra_setup.js
+++ models/extra_setup.js
@@ -4,21 +4,14 @@ const { sequelize } = require("./sequelize");
 const applyExtraSetup = () => {
 
     const {versions, course, messages, qst_in_questionnaires, ans_in_versions, possible_answers,
-           qst_in_versions,questionnaire,scores, test, users, ans_selected_in_test} = sequelize.models;
-        //  book.belongsTo(category, { foreignKey: "cateogry_id", as: "category" });
-        //  book.belongsTo(author, { foreignKey: "author_id", as: "author" });
-        //  author.hasMany(book, { foreignKey: "author_id", as: "books" });
-        //  category.hasMany(book, { foreignKey: "cateogry_id", as: "books" });
-        // Tutorial.hasMany(Comment, { as: "comments" });
-        // Comment.belongsTo(Tutorial, {
-        //   foreignKey: "tutorialId",
-        //   as: "tutorial",
-        // });
-        // message.belongsTo(user)
-        // user.hasMany(message)
-         
-        qst_in_questionnaires.belongsTo(questionnaire, {foreignKey: "questionnaire", as: "questionnaire_id"});
-        questionnaire.hasMany(qst_in_questionnaires,{foreignKey:"questionnaire", as: "questions"});
+           qst_in_versions,questionnaire,scores, test, users, ans_selected_in_test,part_in_questionnaire} = sequelize.models;
+
+
+
+        course.hasMany
+
+        qst_in_questionnaires.belongsTo(part_in_questionnaire, {foreignKey: "part_in_questionnaire", as: "questionnaire_part_id"});
+        part_in_questionnaire.hasMany(qst_in_questionnaires,{foreignKey:"part_in_questionnaire", as: "questions_for_this_part"});
 
         possible_answers.belongsTo(qst_in_questionnaires,{foreignKey:"qst", as: "question"});
         qst_in_questionnaires.hasMany(possible_answers,{foreignKey:"qst", as: "answers" });
@@ -46,7 +39,16 @@ const applyExtraSetup = () => {
         possible_answers.belongsTo(qst_in_questionnaires, {foreignKey:"qst", as: "qst_in_questionnaire_id"} );
         qst_in_questionnaires.hasMany(possible_answers, {foreignKey:"qst", as: "possible_answers"});
 
-        ans_selected_in_test.belongsTo(possible_answers)
+        //this is for the python ai program to use!!!! :)
+        ans_selected_in_test.belongsTo(qst_in_questionnaires, {foreignKey:"qst_in_questionnaire", as: "qst_in_questionnaire_idd"})
+        qst_in_questionnaires.hasMany(ans_selected_in_test, {foreignKey:"qst_in_questionnaire", as:"answers_answered_for_question"})
+
+        ans_selected_in_test.belongsTo(possible_answers, {foreignKey:"ans_selected", as: "question_in_questionnaire"});
+        possible_answers.hasMany(ans_selected_in_test, {foreignKey:"ans_selected", as: "answers_from_tests_that_selected_this"})
+
+
+        part_in_questionnaire.belongsTo(questionnaire, {foreignKey:"questionnaire", as: "questionnaire_id"});
+        questionnaire.hasMany(part_in_questionnaire,{foreignKey:"questionnaire", as: "parts_in_questionnaire"} );
 }
 
 
