5d676af1ce171a7cba69ea9e158935c0c3ae3e2f
Hadas-Lab <36214128472@mby.co.il>
2023-03-10 13:42:13 +0200

    routes->router

--- server.js
+++ server.js
@@ -11,21 +11,21 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
-//routes
+//Router
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 
 //api
-// app.use("/api/auth", require("./routes/authRoutes"))
-app.use('/api/course', require("./routes/courseRoutes"));
-app.use('/api/version', require("./routes/versionRoutes"));
-app.use('/api/score', require("./routes/scoreRoutes"));
-app.use('/api/message', require('./routes/messageRoutes'));
-app.use('/api/possible_answer',require('./routes/possibleAnsRoute'))
-app.use('/api/questionnaire',require('./routes/questionnaireRoutes'))
-app.use('/api/qst_in_questionnaire',require('./routes/qstInQuestionnaireRoutes'))
-app.use('/api/part_in_questionnaire', require('./routes/partInQuestionnaire'));
-app.use('/api/check-test',require('./routes/testRoutes'));
+// app.use("/api/auth", require("./routes/authRouter"))
+app.use('/api/course', require("./routes/courseRouter"));
+app.use('/api/version', require("./routes/versionRouter"));
+app.use('/api/score', require("./routes/scoreRouter"));
+app.use('/api/message', require('./routes/messageRouter'));
+app.use('/api/possible_answer',require('./routes/possibleAnsRouter'))
+app.use('/api/questionnaire',require('./routes/questionnaireRouter'))
+app.use('/api/qst_in_questionnaire',require('./routes/qstInQuestionnaireRouter'))
+app.use('/api/part_in_questionnaire', require('./routes/partRouter'));
+app.use('/api/check-test',require('./routes/testRouter'));
 
 app.all('*', (req, res) => {
     res.status(404)
