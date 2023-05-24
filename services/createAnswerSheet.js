9696275aced3169f2ea022d1451964493273f194
hoandbh <project2023bh@gmail.com>
2023-05-24 21:16:46 +0300

    Finished Excel output :)

--- services/createAnswerSheet.js
+++ services/createAnswerSheet.js
@@ -1,64 +1,77 @@
 // Require library
 const xl = require('excel4node');
 const fs = require('fs')
+const path = require('path');
 
-const Headers = async(dic, ws) =>{
-    for(let l in dic){}
-}
+const fillQuestionNumber = async (numOfQsts, ws, style) => {
 
-const questionNumber = async(numOfQsts, ws)=>{
-    
-    for(let i = 1;i<=numOfQsts;i++){
-        const row = i+1;
-        ws.cell(row,1).string(`question ${i}`);
+    for (let i = 1; i <= numOfQsts; i++) {
+        const row = i + 1;
+        ws.cell(row, 1).number(i).style(style);
     }
 }
+
+const fillOneversion = async (vAnswers, rowN, ws, style1, style2) => {
+    ws.cell(1, rowN).string(`version ${vAnswers.vId}`).style(style1);
+    const ans = vAnswers.answers;
+    const keys = Object.keys(ans);
+    for (let i = 0; i < keys.length; i++) {
+        const value = ans[keys[i]];
+        ws.cell(i + 2, rowN).number(value).style(style2);
+        console.log(`Iteration: ${i},  Value: ${value}`);
+    }
+
+}
+
 class CreateAnswerKey {
 
     createXL = async (ansDic) => {
-        console.log(ansDic);
-        // Create a new instance of a Workbook class
+
         var wb = new xl.Workbook();
 
-        // Add Worksheets to the workbook
-        var ws = wb.addWorksheet('Sheet 1');
+        var ws = wb.addWorksheet('Answers');
 
-        // Create a reusable style
-        var style = wb.createStyle({
+        const boldUnderlinedStyle = wb.createStyle({
             font: {
-                color: '#000000',
-                size: 12,
-            },
-            numberFormat: '$#,##0.00; ($#,##0.00); -',
-        });
+                bold: true,
+                underline: true
 
-        const boldStyle = wb.createStyle({
-            font:{
-                bold:true,
-                underline:true
-                
             }
         });
         const underlinedStyle = wb.createStyle({
-            font:{underline:true}
+            font: { underline: true }
         });
+        const boldStyle = wb.createStyle({
+            font: {
+                bold: true,
+            },
+            alignment: {
+                horizontal: 'center'
+            }
+        });
+        const boldStyleSide = wb.createStyle({
+            font: {
+                bold: true,
+            }
+        })
 
-        ws.cell(1,1).string('Question Number').style(underlinedStyle);
-        for(let i = 0;i<ansDic.length;i++){
-            const col = i+2;
+        ws.cell(1, 1).string('Question Number').style(underlinedStyle);
 
-            const answers = ansDic[i].answers;
+        const numOfQsts = Object.keys(ansDic[0].answers).length;
 
-            ws.cell(1,col).string(`version ${ansDic[i].vId}`).style(boldStyle);
+        await (fillQuestionNumber(numOfQsts, ws, boldStyleSide));
 
+        for (let i = 0; i < ansDic.length; i++) {
+            fillOneversion(ansDic[i], i + 2, ws, boldUnderlinedStyle, boldStyle);
         }
-        await(questionNumber(14,ws));
 
-
-        wb.writeToBuffer().then((buffer)=>{
-            fs.writeFileSync(`./public/files/Excel.xlsx`,buffer);
-        });
-        wb.write()
+        const filePath = path.join(__dirname, `../public/files/answerDics`, 'output.xlsx');
+        try {
+            fs.accessSync(filePath, fs.constants.W_OK);
+            wb.write(filePath);
+        } catch (error) {
+            console.error(`Error occurred while writing to the file: ${error.message}`);
+        }
     }
 }
 
